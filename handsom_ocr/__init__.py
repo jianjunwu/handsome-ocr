ab24e132fd8f88ae3d5b842634875f4e6040145b
吴建军 <jianjun.wu@brilliant-solution.com>
2020-06-23 23:14:48 +0800

    init repo

---
+++ handsom_ocr/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2020/06/23 23:10:47
+@Author  :   nicholas wu 
+@Version :   1.0
+@Contact :   nicholas_wu@aliyun.com
+@License :    
+@Desc    :   None
+'''
